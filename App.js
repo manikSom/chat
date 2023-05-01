0881a76d32fba1ee8003b7295c649998f38a7f14
Manickam Somasundaram <manickam.som@gmail.com>
2023-05-01 18:31:07 +0200

    async and offline functionality added

--- App.js
+++ App.js
@@ -2,20 +2,28 @@ import React, { Component } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { Alert, StyleSheet } from "react-native";
+import {
+    getFirestore,
+    disableNetwork,
+    enableNetwork,
+} from "firebase/firestore";
 import Start from './components/Start';
 import Chat from './components/Chat';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useNetInfo } from "@react-native-community/netinfo";
+import { useEffect } from "react";
 
 const Stack = createNativeStackNavigator();
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-apiKey: "AIzaSyDqVh0VwW7MaGBzrfWAJ-UbiWJFtN5QLT8",
-authDomain: "chatapp-e3906.firebaseapp.com",
-projectId: "chatapp-e3906",
-storageBucket: "chatapp-e3906.appspot.com",
-messagingSenderId: "425165476609",
-appId: "1:425165476609:web:f9fe23dbf39aa3753fc100"
+    apiKey: "AIzaSyDqVh0VwW7MaGBzrfWAJ-UbiWJFtN5QLT8",
+    authDomain: "chatapp-e3906.firebaseapp.com",
+    projectId: "chatapp-e3906",
+    storageBucket: "chatapp-e3906.appspot.com",
+    messagingSenderId: "425165476609",
+    appId: "1:425165476609:web:f9fe23dbf39aa3753fc100"
 };
 
 // Initialize Firebase
@@ -25,16 +33,31 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 const App = () => {
+    const connectionStatus = useNetInfo();
+    useEffect(() => {
+        if (connectionStatus.isConnected === false) {
+            Alert.alert("Connection Lost!");
+            disableNetwork(db);
+        } else if (connectionStatus.isConnected === true) {
+            enableNetwork(db);
+        }
+    }, [connectionStatus.isConnected]);
     return (
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName='Start'>
-          <Stack.Screen name='Start' component={Start} />
-          <Stack.Screen name='Chat'>
-          {(props) => <Chat db={db} {...props} />}
-        </Stack.Screen>
-        </Stack.Navigator>
-      </NavigationContainer>
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName='Start'>
+                <Stack.Screen name='Start' component={Start} />
+                <Stack.Screen name='Chat'>
+                    {(props) => (
+                        <Chat
+                            isConnected={connectionStatus.isConnected}
+                            db={db}
+                            {...props}
+                        />
+                    )}
+                </Stack.Screen>
+            </Stack.Navigator>
+        </NavigationContainer>
     );
-  };
-  
-  export default App;
+};
+
+export default App;
--- components/Chat.js
+++ components/Chat.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { KeyboardAvoidingView, StyleSheet, Text, View } from "react-native";
-import { Bubble, GiftedChat } from "react-native-gifted-chat";
+import { Bubble, GiftedChat, InputToolbar } from "react-native-gifted-chat";
 import {
     collection,
     addDoc,
@@ -8,30 +8,48 @@ import {
     query,
     orderBy,
 } from "firebase/firestore";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const Chat = ({ db, route, navigation }) => {
+const Chat = ({ isConnected, db, route, navigation }) => {
     const { name, color, userID } = route.params;
     const [messages, setMessages] = useState([]);
+    let unsubMessages;
 
     useEffect(() => {
         navigation.setOptions({ title: name });
-        const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
-        const unsubMessages = onSnapshot(q, (docs) => {
-            let newMessages = [];
-            docs.forEach((doc) => {
-                newMessages.push({
-                    id: doc.id,
-                    ...doc.data(),
-                    createdAt: new Date(doc.data().createdAt.toMillis()),
+        if (isConnected === true) {
+            if (unsubMessages) unsubMessages();
+            unsubMessages = null;
+            const q = query(collection(db, "messages"), orderBy("createdAt", "desc"));
+            unsubMessages = onSnapshot(q, (docs) => {
+                let newMessages = [];
+                docs.forEach((doc) => {
+                    newMessages.push({
+                        id: doc.id,
+                        ...doc.data(),
+                        createdAt: new Date(doc.data().createdAt.toMillis()),
+                    });
                 });
+                cacheMessages(newMessages);
+                setMessages(newMessages);
             });
-            setMessages(newMessages);
-        });
+        } else loadCachedMessages();
         return () => {
             if (unsubMessages) unsubMessages();
         };
 
-    }, []);
+    }, [isConnected]);
+    const loadCachedMessages = async () => {
+        const cachedMessages = (await AsyncStorage.getItem("messages")) || [];
+        setMessages(JSON.parse(cachedMessages));
+    };
+    const cacheMessages = async (messagesToCache) => {
+        try {
+            await AsyncStorage.setItem("messages", JSON.stringify(messagesToCache));
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
 
     const addMessagesItem = async (newMessage) => {
         const newMessageRef = await addDoc(
@@ -48,6 +66,15 @@ const Chat = ({ db, route, navigation }) => {
     const onSend = (newMessages) => {
         addMessagesItem(newMessages);
     };
+
+    const renderInputToolbar = (props) => {
+        if (isConnected) {
+            return <InputToolbar {...props} />;
+        } else {
+            return null;
+        }
+    };
+
 
     const renderBubble = (props) => {
         return (
@@ -76,6 +103,7 @@ const Chat = ({ db, route, navigation }) => {
                 style={styles.textingBox}
                 messages={messages}
                 renderBubble={renderBubble}
+                renderInputToolbar={renderInputToolbar}
                 onSend={(messages) => onSend(messages)}
                 user={{
                     _id: userID,
